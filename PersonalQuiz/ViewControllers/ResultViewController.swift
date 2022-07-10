183ba529b68aff44416439d346d75999476c9b42
TheRenat <therenat@mail.ru>
2022-07-10 03:20:38 +0300

    add logic

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -12,20 +12,45 @@ class ResultViewController: UIViewController {
     var answers: [Answer] = []
     
     
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         
-        var animals = answers.compactMap { $0.animal }
+        let animals = answers.compactMap { $0.animal }
+        let countedSet = NSCountedSet(array: animals)
+        let mostFrequent = countedSet.max { countedSet.count(for: $0) < countedSet.count(for: $1) }
         
-        var array: [String] = []
+        print(mostFrequent)
+        
+        var dogs: [String] = []
+        var cats: [Animal] = []
+        var turtles: [Animal] = []
+        var rabbits: [Animal] = []
         
         for animal in animals {
             if animal == Animal.dog {
-                array
+                dogs.append("dog")
+            } else if animal == Animal.cat {
+                cats.append(animal)
+            } else if animal == Animal.turtle {
+                turtles.append(animal)
+            } else if animal == Animal.rabbit {
+                rabbits.append(animal)
+            }
+        }
+        
+        if mostFrequent as? Animal == Animal.dog {
+            if let emoji = mostFrequent as? String {
+                emojiLabel.text = answers.description
             }
         }
     }
+    
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
